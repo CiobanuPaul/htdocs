f1ca6424d45a65566d1d839a17529948ab73c009
CiobanuPaul <ciobanuioanpaul43@gmail.com>
2023-11-15 16:45:30 +0200

    first commit

---
+++ home.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+    <title>
+        Activități-FMI
+    </title>
+</head>
+<body>
+    <nav>
+        <div>Acasa</div>
+        <div>Inregistrare</div>
+        <div>Ceva</div>
+    <nav>
+    
+<main>
+    <h2>Activitățile Facultății de Matematica și Informatică</h2>
+
+    <p>Site-ul va permite inregistrarea si autentificarea a trei tipuri de utilizatori: studenti, profesori si candidati.</p>
+    <p>Studentii vor avea acces la informatii despre orar, formatiuni de studii, si diverse activitati ce sunt realizate 
+    exclusiv pentru pentru studenti sau si pentru studenti si pentru profesori. </p>
+    <p>Profesorii vor avea acces la informatii asemanatoare, dar specifice profesorilor sau valabile si pentru studenti si pentru profesori.</p>
+    <p>Candidatii nu sunt inmatriculati, dar ar si-au depus dosar pentru admitere. Acestia vor primi in continuare
+    pe site informatii despre procesul de admitere</p>
+    <p>Pentru fiecare activitate va exista un formular ce permite inscrierea unui utilizator la activitate.</p>
+    
+    <img src="diag.png"></img>
+    
+
+</main>
+</body>
+</html>
